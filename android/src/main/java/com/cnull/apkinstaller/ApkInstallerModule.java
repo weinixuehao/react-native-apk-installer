65d2acb782a5d44b5f3e556d1321591f3f8722ff
wt_02 <long.chen>
2018-11-23 11:34:26 +0800

    .

--- android/src/main/java/com/cnull/apkinstaller/ApkInstallerModule.java
+++ android/src/main/java/com/cnull/apkinstaller/ApkInstallerModule.java
@@ -54,20 +54,34 @@ public class ApkInstallerModule extends ReactContextBaseJavaModule {
       } catch (Exception e) {
           e.printStackTrace();
       }
-      Uri apkURI;
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-          apkURI = ApkInstallerFileProvider.getUriForFile(_context,
-                  _context.getPackageName() + ".provider",
-            new File(Environment.getExternalStorageDirectory(), path));
-      } else {
-          apkURI = Uri.fromFile(new File(path));
+//      Uri apkURI;
+//      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//          apkURI = ApkInstallerFileProvider.getUriForFile(_context,
+//                  _context.getPackageName() + ".provider",
+//            new File(path));
+//      } else {
+//          apkURI = Uri.fromFile(new File(path));
+//      }
+//      Intent intent = new Intent(Intent.ACTION_VIEW);
+//      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//      intent.setDataAndType(apkURI, "application/vnd.android.package-archive");
+//      _context.startActivity(intent);
+      if(Build.VERSION.SDK_INT>=24) {//判读版本是否在7.0以上
+          File file= new File(path);
+          Uri apkUri = ApkInstallerFileProvider.getUriForFile(_context, _context.getPackageName() + ".provider", file);//在AndroidManifest中的android:authorities值
+          if (BuildConfig.DEBUG) {
+              Log.w(ApkInstallerModule.class.getSimpleName(), "packageName:" + _context.getPackageName() + " apkUri:"+apkUri.getPath() + " path:"+path);
+          }
+          Intent install = new Intent(Intent.ACTION_VIEW);
+          install.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+          install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);//添加这一句表示对目标应用临时授权该Uri所代表的文件
+          install.setDataAndType(apkUri, "application/vnd.android.package-archive");
+          _context.startActivity(install);
+      } else{
+          Intent install = new Intent(Intent.ACTION_VIEW);
+          install.setDataAndType(Uri.fromFile(new File(path)), "application/vnd.android.package-archive");
+          install.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+          _context.startActivity(install);
       }
-      if (BuildConfig.DEBUG) {
-          Log.w(ApkInstallerModule.class.getSimpleName(), "packageName:" + _context.getPackageName() + " apkURI:"+apkURI.getPath() + " path:"+path);
-      }
-      Intent intent = new Intent(Intent.ACTION_VIEW);
-      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      intent.setDataAndType(apkURI, "application/vnd.android.package-archive");
-      _context.startActivity(intent);
   }
 }
